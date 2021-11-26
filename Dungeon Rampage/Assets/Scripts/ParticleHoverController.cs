8399d91c9f904cf4f9254a8b2d2330ddedabe9a8
Joseph Collard <jcollard@chadwickschool.org>
2021-11-26 15:32:08 -0800

    feat: Add image for shops / rest.

--- Dungeon Rampage/Assets/Scripts/ParticleHoverController.cs
+++ Dungeon Rampage/Assets/Scripts/ParticleHoverController.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class ParticleHoverController : MonoBehaviour
 {
     public ParticleSystem particles;
+    public string Message;
     public void OnMouseEnter()
     {
         this.particles.gameObject.SetActive(true);
+        if (Message != null && Message != string.Empty)
+        {
+            MessageController.Instance.DisplayMessage(Message);
+            Message = string.Empty;
+        }
     }
 
     public void OnMouseExit()
